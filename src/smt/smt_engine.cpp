9cc6041741e3711bded76faab01cee58d213beb7
ajreynol <andrew.j.reynolds@gmail.com>
2019-10-29 11:06:02 -0500

    Apply core connective algo

--- src/smt/smt_engine.cpp
+++ src/smt/smt_engine.cpp
@@ -1300,6 +1300,12 @@ void SmtEngine::setDefaults() {
     }
   }
 
+  // sygus core connective requires unsat cores
+  if (options::sygusCoreConnective())
+  {
+    options::unsatCores.set(true);
+  }
+
   if ((options::checkModels() || options::checkSynthSol()
        || options::produceAbducts()
        || options::modelCoresMode() != MODEL_CORES_NONE
