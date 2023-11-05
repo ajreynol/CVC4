e5fe4e924dcc38e06696c3fd6274c398d37da4ae
ajreynol <andrew.j.reynolds@gmail.com>
2023-11-04 19:50:36 -0500

    More

--- src/theory/quantifiers/eager/cd_tnode_trie.cpp
+++ src/theory/quantifiers/eager/cd_tnode_trie.cpp
@@ -15,26 +15,75 @@
 
 #include "theory/quantifiers/eager/cd_tnode_trie.h"
 
+#include "theory/quantifiers/quantifiers_state.h"
+
 namespace cvc5::internal {
 namespace theory {
 namespace quantifiers {
 
 CDTNodeTrie::CDTNodeTrie(context::Context* c)
-    : d_reps(c), d_repChildren(c), d_repSize(c, 0)
+    : d_data(c), d_repMap(c), d_repSize(c, 0), d_toMerge(c), d_toMergeProcessed(c, 0)
 {
 }
 
-void CDTNodeTrie::add(CDTNodeTrieAllocator* a,
+void CDTNodeTrie::clear()
+{
+  d_data = TNode::null();
+  d_repSize = 0;
+  d_toMergeProcessed = d_toMerge.size();
+}
+
+void CDTNodeTrie::add(CDTNodeTrieAllocator* al,
                       const std::vector<TNode>& args,
                       TNode t)
 {
+  CDTNodeTrie* cur = this;
+  context::CDHashMap<TNode, size_t>::const_iterator it;
+  for (TNode a : args)
+  {
+    it = cur->d_repMap.find(a);
+    if (it == cur->d_repMap.end())
+    {
+      cur = cur->push_back(al, a);
+    }
+    else
+    {
+      cur = cur->d_repChildren[it->second];
+    }
+  }
+  if (!cur->hasData())
+  {
+    // just set the data, without constructing child
+    cur->d_data = t;
+  }
 }
 
-CDTNodeTrieAllocator::CDTNodeTrieAllocator(context::Context* c) : d_ctx(c) {}
+CDTNodeTrie* CDTNodeTrie::push_back(CDTNodeTrieAllocator* al, TNode r)
+{
+  // TODO: optimization, can fill in empty child that was disabled?
+  CDTNodeTrie* ret;
+  if (d_repSize<d_repChildren.size())
+  {
+    ret = d_repChildren[d_repSize];
+    ret->clear();
+  }
+  else
+  {
+    ret = al->alloc();
+    d_repChildren.push_back(ret);
+  }
+  Assert (d_repChildren[d_repSize]==ret);
+  ret->d_data = r;
+  d_repMap[r] = d_repSize;
+  d_repSize = d_repSize + 1;
+  return ret;
+}
+
+CDTNodeTrieAllocator::CDTNodeTrieAllocator(context::Context* c) : d_ctx(c), d_alloc(c) {}
 
 CDTNodeTrie* CDTNodeTrieAllocator::alloc()
 {
-  d_alloc.emplace_back(std::unique_ptr<CDTNodeTrie>(new CDTNodeTrie(d_ctx)));
+  d_alloc.push_back(std::shared_ptr<CDTNodeTrie>(new CDTNodeTrie(d_ctx)));
   return d_alloc.back().get();
 }
 
@@ -43,20 +92,126 @@ CDTNodeTrieIterator::CDTNodeTrieIterator(CDTNodeTrieAllocator* a,
                                          CDTNodeTrie* cdtnt)
     : d_alloc(a), d_qs(qs)
 {
-  setActive({cdtnt});
+  d_stack.emplace_back(d_alloc, qs, cdtnt);
 }
-TNode CDTNodeTrieIterator::pushNextChild() { return d_null; }
-bool CDTNodeTrieIterator::push(TNode r) { return false; }
-void CDTNodeTrieIterator::pop() {}
+
+TNode CDTNodeTrieIterator::pushNextChild()
+{
+  Assert (!d_stack.empty());
+  StackFrame& sf = d_stack.back();
+  if (sf.isFinished())
+  {
+    return d_null;
+  }
+  CDTNodeTrie* next;
+  TNode ret;
+  ret = sf.d_cit->first;
+  next = sf.d_cit->second;
+  ++sf.d_cit;
+  d_stack.emplace_back(d_alloc, d_qs, next);
+  return ret;
+}
+
+bool CDTNodeTrieIterator::push(TNode r) {
+  Assert (!d_stack.empty());
+  StackFrame& sf = d_stack.back();
+  std::map<TNode, CDTNodeTrie*>::iterator it = sf.d_curChildren.find(r);
+  if (it ==sf.d_curChildren.end())
+  {
+    return false;
+  }
+  d_stack.emplace_back(d_alloc, d_qs, it->second);
+  return true;
+}
+
+void CDTNodeTrieIterator::pop() {
+  Assert (!d_stack.empty());
+  d_stack.pop_back();
+}
+
 TNode CDTNodeTrieIterator::getData()
 {
+  Assert (!d_stack.empty());
   Assert(d_active != nullptr);
   Assert(d_active->hasData());
-  return d_active->getData();
+  return d_stack.back().d_active->getData();
 }
-void CDTNodeTrieIterator::setActive(const std::vector<CDTNodeTrie*>& active)
+
+CDTNodeTrieIterator::StackFrame::StackFrame(CDTNodeTrieAllocator* al, QuantifiersState& qs, CDTNodeTrie* active)
 {
-  Assert(!active.empty());
+  d_active = active;
+  std::map<TNode, CDTNodeTrie*>::iterator it;
+  // process and merge the children
+  CDTNodeTrie* cur;
+  std::vector<CDTNodeTrie*> process;
+  process.emplace_back(active);
+  do
+  {
+    cur = process.back();
+    process.pop_back();
+    for (size_t i=0, nreps = cur->d_repSize; i<nreps; i++)
+    {
+      CDTNodeTrie* cc = cur->d_repChildren[i];
+      TNode n = cc->d_data;
+      if (n.isNull())
+      {
+        // already merged
+        continue;
+      }
+      TNode r = qs.getRepresentative(n);
+      it = d_curChildren.find(r);
+      // if we have yet to see this representative
+      if (it==d_curChildren.end())
+      {
+        if (cur==active)
+        {
+          // if the original, we are done
+          d_curChildren[r] = cc;
+          if (n!=r)
+          {
+            // keep the same but need to replace the representative, note that
+            // n is stale and not cleaned up
+            cc->d_data = r;
+            cc->d_repMap[r] = i;
+          }
+        }
+        else
+        {
+          // must add to active
+          CDTNodeTrie* ccn = active->push_back(al, r);
+          ccn->d_toMerge.push_back(cc);
+          d_curChildren[r] = ccn;
+        }
+        i++;
+      }
+      else
+      {
+        // mark that it will need to be merged
+        it->second->d_toMerge.push_back(cc);
+        if (cur==active)
+        {
+          // if we are active, we must mark this as disabled
+          cc->d_data = TNode::null();
+        }
+      }
+    }
+    // process those waiting to merge with this
+    size_t ntomerge = cur->d_toMerge.size();
+    size_t i = cur->d_toMergeProcessed.get();
+    if (i<ntomerge)
+    {
+      do
+      {
+        process.emplace_back(cur->d_toMerge[i]);
+        i++;
+      }while (i<ntomerge);
+      // we've processed all
+      cur->d_toMergeProcessed = ntomerge;
+    }
+  }while (!process.empty());
+
+
+  d_cit = d_curChildren.begin();
 }
 
 }  // namespace quantifiers
--- src/theory/quantifiers/eager/cd_tnode_trie.h
+++ src/theory/quantifiers/eager/cd_tnode_trie.h
@@ -18,6 +18,7 @@
 #ifndef CVC5__THEORY__QUANTIFIERS__EAGER__CD_TNODE_TRIE_H
 #define CVC5__THEORY__QUANTIFIERS__EAGER__CD_TNODE_TRIE_H
 
+#include "context/cdhashmap.h"
 #include "context/cdlist.h"
 #include "context/cdo.h"
 #include "expr/node.h"
@@ -33,18 +34,28 @@ class CDTNodeTrie
 {
  public:
   CDTNodeTrie(context::Context* c);
+  context::CDO<TNode> d_data;
   /** List of reps and children, possible stale */
-  context::CDList<TNode> d_reps;
+  context::CDHashMap<TNode, size_t> d_repMap;
   /** The children */
-  context::CDList<CDTNodeTrie*> d_repChildren;
+  std::vector<CDTNodeTrie*> d_repChildren;
   /** The size of d_reps that is valid */
   context::CDO<size_t> d_repSize;
+  /** To merge */
+  context::CDList<CDTNodeTrie*> d_toMerge;
+  /** To merge index */
+  context::CDO<size_t> d_toMergeProcessed;
+  /** clear */
+  void clear();
   /** Adds term without cleaning */
-  void add(CDTNodeTrieAllocator* a, const std::vector<TNode>& args, TNode t);
+  void add(CDTNodeTrieAllocator* al, const std::vector<TNode>& args, TNode t);
   /** For leaf nodes : does this node have data? */
-  bool hasData() const { return !d_reps.empty(); }
+  bool hasData() const { return !d_data.get().isNull(); }
   /** For leaf nodes : get the node corresponding to this leaf. */
-  TNode getData() const { return *d_reps.begin(); }
+  TNode getData() const {
+    return d_data.get();
+}
+  CDTNodeTrie* push_back(CDTNodeTrieAllocator* al, TNode r);
 };
 
 class CDTNodeTrieAllocator
@@ -56,7 +67,7 @@ class CDTNodeTrieAllocator
 
  private:
   context::Context* d_ctx;
-  std::vector<std::unique_ptr<CDTNodeTrie>> d_alloc;
+  context::CDList<std::shared_ptr<CDTNodeTrie>> d_alloc;
 };
 
 /**
@@ -65,7 +76,7 @@ class CDTNodeTrieAllocator
 class CDTNodeTrieIterator
 {
  public:
-  CDTNodeTrieIterator(CDTNodeTrieAllocator* a,
+  CDTNodeTrieIterator(CDTNodeTrieAllocator* al,
                       QuantifiersState& qs,
                       CDTNodeTrie* cdtnt);
   /** Get the next child in this trie and push it */
@@ -78,12 +89,18 @@ class CDTNodeTrieIterator
   TNode getData();
 
  private:
-  void setActive(const std::vector<CDTNodeTrie*>& active);
   CDTNodeTrieAllocator* d_alloc;
   QuantifiersState& d_qs;
-  CDTNodeTrie* d_active;
-  std::vector<std::pair<TNode, std::vector<CDTNodeTrie*>>> d_curChildren;
-  size_t d_cindex;
+  class StackFrame
+  {
+  public:
+    StackFrame(CDTNodeTrieAllocator* al, QuantifiersState& qs, CDTNodeTrie* active);
+    CDTNodeTrie* d_active;
+    std::map<TNode, CDTNodeTrie*> d_curChildren;
+    std::map<TNode, CDTNodeTrie*>::iterator d_cit;
+    bool isFinished() const { return d_cit==d_curChildren.end(); }
+  };
+  std::vector<StackFrame> d_stack;
   Node d_null;
 };
 
