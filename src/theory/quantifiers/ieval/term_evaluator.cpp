b115b3d9e04dd1ed6a8949e8ebe779636a4607fc
ajreynol <andrew.j.reynolds@gmail.com>
2023-11-06 15:51:32 -0600

    fix

--- src/theory/quantifiers/ieval/term_evaluator.cpp
+++ src/theory/quantifiers/ieval/term_evaluator.cpp
@@ -369,7 +369,7 @@ TNode TermEvaluatorEntailed::evaluate(const State& s,
       {
         Trace("ieval-state-debug")
             << "...some child of evaluated term" << std::endl;
-        return ret;
+        return s.getNone();
       }
     }
     Node preTerm;
