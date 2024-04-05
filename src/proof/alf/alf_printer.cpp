0a83052390cfb3c9761e2f2a9382f492dee41bae
ajreynol <andrew.j.reynolds@gmail.com>
2024-04-05 14:10:27 -0500

    Allow trust

--- src/proof/alf/alf_printer.cpp
+++ src/proof/alf/alf_printer.cpp
@@ -746,6 +746,10 @@ void AlfPrinter::printStepPost(AlfPrintChannel* out, const ProofNode* pn)
   // if we don't handle the rule, print trust
   if (!handled)
   {
+    if (!options().proof.alfAllowTrust)
+    {
+      Unreachable() << "An ALF proof equires a trust step for " << pn->getRule() << ", but --alf-allow-trust is false" << std::endl;
+    }
     out->printTrustStep(pn->getRule(),
                         conclusionPrint,
                         id,
