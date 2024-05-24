511ef75986e06de369f855fb2ccfc80736408659
ajreynol <andrew.j.reynolds@gmail.com>
2024-05-23 19:54:13 -0500

    Try

--- src/proof/alf/alf_printer.cpp
+++ src/proof/alf/alf_printer.cpp
@@ -246,7 +246,8 @@ bool AlfPrinter::isHandledTheoryRewrite(ProofRewriteRule id,
     case ProofRewriteRule::STR_IN_RE_SIGMA:
     case ProofRewriteRule::STR_IN_RE_CONSUME:
     case ProofRewriteRule::SETS_IS_EMPTY_EVAL:
-    case ProofRewriteRule::RE_INTER_UNION_INCLUSION: return true;
+    case ProofRewriteRule::RE_INTER_UNION_INCLUSION:
+    case ProofRewriteRule::BV_BITWISE_SLICING: return true;
     case ProofRewriteRule::STR_IN_RE_EVAL:
       Assert(n[0].getKind() == Kind::STRING_IN_REGEXP && n[0][0].isConst());
       return canEvaluateRegExp(n[0][1]);
