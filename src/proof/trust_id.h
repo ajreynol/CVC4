04c471ef1d81e7fa4e78105a3168a5ef4c8f6eaa
ajreynol <andrew.j.reynolds@gmail.com>
2024-12-18 11:35:01 -0600

    Minro

--- src/proof/trust_id.h
+++ src/proof/trust_id.h
@@ -154,7 +154,7 @@ enum class TrustId : uint32_t
   ARITH_NL_COMPARE_LIT_TRANSFORM,
   /** A lemma from the DIO solver */
   ARITH_DIO_LEMMA,
-  /** A nonlinear comparison lemma */
+  /** A nonlinear comparison lemma that failed to explain */
   ARITH_NL_COMPARE_LEMMA,
   /** A conflict coming from the bitblast solver */
   BV_BITBLAST_CONFLICT,
