adeeacaf4894b2d6fbd2eec717aa421a12c3def0
ajreynol <andrew.j.reynolds@gmail.com>
2025-01-06 16:25:20 -0600

    Minor

--- src/rewriter/basic_rewrite_rcons.cpp
+++ src/rewriter/basic_rewrite_rcons.cpp
@@ -1151,7 +1151,7 @@ bool BasicRewriteRCons::ensureProofMacroQuantVarElimEq(CDProof* cdp,
       // step will be unsound. this is the case e.g. when
       // a != (str.++ b x) is turned into x != (str.substr a (str.len b) ...)
       // where the latter implies the former, but they are not equivalent
-      if (rewrite(body1r[i]) != rewrite(body1re[i]))
+      if (extendedRewrite(body1r[i]) != extendedRewrite(body1re[i]))
       {
         Trace("brc-macro") << "...failed to rewrite" << std::endl;
         return false;
