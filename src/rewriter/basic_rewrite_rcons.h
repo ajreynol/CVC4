c4813696c363ef858600b2b66d70fbfba823b659
ajreynol <andrew.j.reynolds@gmail.com>
2025-02-19 10:20:25 -0600

    More

--- src/rewriter/basic_rewrite_rcons.h
+++ src/rewriter/basic_rewrite_rcons.h
@@ -418,28 +418,6 @@ class BasicRewriteRCons : protected EnvObj
   Node proveSymm(CDProof* cdp, const Node& eq);
   /**
    * Prove congruence for left hand side term n.
-   * If n is a term of the form (f t1 ... tn), this proves (f s1 .... sn)
-   * where ti != si iff premises[i] is non-null and the equality (= ti si).
-   * Note that we permit providing null premises[i] in which case si is ti
-   * and we prove (= ti ti) by REFL.
-   */
-  Node proveCong(CDProof* cdp,
-                 const Node& n,
-                 const std::vector<Node>& premises);
-  /**
-   * Assuming cdp has proofs of (=> A B) and (=> B A), this ensures we
-   * have a proof of (= A B).
-   */
-  Node proveDualImplication(CDProof* cdp,
-                            const Node& impl,
-                            const Node& implRev);
-  /**
-   * Prove symmetry of equality eq, in particular this proves eq[1] == eq[0]
-   * where eq is an equality and adds it to cdp.
-   */
-  Node proveSymm(CDProof* cdp, const Node& eq);
-  /**
-   * Prove congruence for left hand side term n.
    * If n is a term of the form (f t1 ... tn), this proves
    *  (= (f t1 ... sn) (f s1 .... sn))
    * where si is different from ti iff premises[i] is the equality (= ti si).
