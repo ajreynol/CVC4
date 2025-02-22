939b2e359e01e040416d5bae1b07723dc829608e
ajreynol <andrew.j.reynolds@gmail.com>
2025-02-22 08:02:45 -0600

    Simplify rules

--- src/theory/bv/macro_rewrite_elaborator.cpp
+++ src/theory/bv/macro_rewrite_elaborator.cpp
@@ -15,6 +15,12 @@
 
 #include "theory/bv/macro_rewrite_elaborator.h"
 
+#include "expr/aci_norm.h"
+#include "smt/env.h"
+#include "proof/proof_node_algorithm.h"
+#include "proof/proof_node_manager.h"
+#include "proof/proof_checker.h"
+
 namespace cvc5::internal {
 namespace theory {
 namespace bv {
@@ -28,11 +34,12 @@ bool MacroRewriteElaborator::ensureProofFor(CDProof* cdp,
   Trace("bv-rew-elab") << "ensureProofFor: " << id << " " << eq << std::endl;
   switch (id)
   {
+    case ProofRewriteRule::MACRO_BV_OR_SIMPLIFY:
+    case ProofRewriteRule::MACRO_BV_AND_SIMPLIFY:
+      return ensureProofForSimplify(cdp, eq);
     case ProofRewriteRule::MACRO_BV_EXTRACT_CONCAT:
     case ProofRewriteRule::MACRO_BV_EXTRACT_SIGN_EXTEND:
     case ProofRewriteRule::MACRO_BV_ASHR_BY_CONST:
-    case ProofRewriteRule::MACRO_BV_OR_SIMPLIFY:
-    case ProofRewriteRule::MACRO_BV_AND_SIMPLIFY:
     case ProofRewriteRule::MACRO_BV_XOR_SIMPLIFY:
     case ProofRewriteRule::MACRO_BV_AND_OR_XOR_CONCAT_PULLUP:
     case ProofRewriteRule::MACRO_BV_MULT_SLT_MULT:
@@ -42,6 +49,89 @@ bool MacroRewriteElaborator::ensureProofFor(CDProof* cdp,
     default: break;
   }
   return false;
+}
+
+bool MacroRewriteElaborator::ensureProofForSimplify(CDProof* cdp, const Node& eq)
+{
+  NodeManager * nm = nodeManager();
+  Kind k = eq[0].getKind();
+  std::vector<Node> consts;
+  std::vector<Node> nconsts;
+  for (const Node& cc : eq[0])
+  {
+    if (cc.isConst())
+    {
+      consts.push_back(cc);
+    }
+    else
+    {
+      nconsts.push_back(cc);
+    }
+  }
+  if (consts.size()<=1 || nconsts.empty())
+  {
+    return false;
+  }
+  std::vector<Node> transEq;
+  Node cc = nm->mkNode(k, consts);
+  nconsts.insert(nconsts.begin(), cc);
+  Node eq0c = nm->mkNode(k, nconsts);
+  Node equiv1 = eq[0].eqNode(eq0c);
+  cdp->addStep(equiv1, ProofRule::ACI_NORM, {}, {equiv1});
+  transEq.push_back(equiv1);
+  Node ccr = rewrite(cc);
+  Node ceq = cc.eqNode(ccr);
+  cdp->addTrustedStep(ceq, TrustId::MACRO_THEORY_REWRITE_RCONS, {}, {});
+  std::vector<Node> premises;
+  premises.push_back(ceq);
+  Node equiv2 = proveCong(cdp, eq0c, premises);
+  transEq.push_back(equiv2);
+  if (equiv2[1]!=eq[1])
+  {
+    if (expr::isACINorm(equiv2[1], eq[1]))
+    {
+      Node equiv3 = equiv2[1].eqNode(eq[1]);
+      cdp->addStep(equiv3, ProofRule::ACI_NORM, {}, {equiv3});
+      transEq.push_back(equiv3);
+    }
+    else
+    {
+      return false;
+    }
+  }
+  cdp->addStep(eq, ProofRule::TRANS, transEq, {});
+  return true;
+}
+
+
+Node MacroRewriteElaborator::proveCong(CDProof* cdp,
+                                  const Node& n,
+                                  const std::vector<Node>& premises)
+{
+  std::vector<Node> cpremises = premises;
+  std::vector<Node> cargs;
+  ProofRule cr = expr::getCongRule(n, cargs);
+  cpremises.resize(n.getNumChildren());
+  // add REFL if a premise is not provided
+  for (size_t i = 0, npremises = cpremises.size(); i < npremises; i++)
+  {
+    if (cpremises[i].isNull())
+    {
+      Node refl = n[i].eqNode(n[i]);
+      cdp->addStep(refl, ProofRule::REFL, {}, {n[i]});
+      cpremises[i] = refl;
+    }
+  }
+  Trace("brc-macro") << "- cong " << cr << " " << cpremises << " " << cargs
+                     << std::endl;
+  ProofChecker* pc = d_env.getProofNodeManager()->getChecker();
+  Node eq = pc->checkDebug(cr, cpremises, cargs);
+  Trace("brc-macro") << "...returns " << eq << std::endl;
+  if (!eq.isNull())
+  {
+    cdp->addStep(eq, cr, cpremises, cargs);
+  }
+  return eq;
 }
 
 }  // namespace bv
