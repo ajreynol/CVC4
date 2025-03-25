abb3509ddf194b492db02611d74abd0c86df00a9
ajreynol <andrew.j.reynolds@gmail.com>
2025-03-25 15:02:07 -0500

    More improvements

--- include/cvc5/cvc5_proof_rule.h
+++ include/cvc5/cvc5_proof_rule.h
@@ -4431,6 +4431,8 @@ enum ENUM(ProofRewriteRule)
   EVALUE(STR_INDEXOF_SELF),
   /** Auto-generated from RARE rule str-indexof-no-contains */
   EVALUE(STR_INDEXOF_NO_CONTAINS),
+  /** Auto-generated from RARE rule str-indexof-oob2 */
+  EVALUE(STR_INDEXOF_OOB2),
   /** Auto-generated from RARE rule str-indexof-oob */
   EVALUE(STR_INDEXOF_OOB),
   /** Auto-generated from RARE rule str-indexof-contains-pre */
